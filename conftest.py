0ee533018d5bc3b33d74c0c4d6ef8649b6da583e
FILA <fila090580@gmail.com>
2025-03-17 01:50:05 +0200

    new

--- conftest.py
+++ conftest.py
@@ -2,15 +2,15 @@ import pytest
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
-@pytest.fixture(scope="session")
-def browser_context_args(browser_context_args):
-    return {
-
-        "viewport": {
-            "width": 1920,
-            "height": 1080,
-        }
-    }
+# @pytest.fixture(scope="session")
+# def browser_context_args(browser_context_args):
+#     return {
+#
+#         "viewport": {
+#             "width": 1920,
+#             "height": 1080,
+#         }
+#     }
 
 
 @pytest.fixture
--- test_todo.py
+++ test_todo.py
@@ -15,13 +15,25 @@ def test_add_todo(page: Page) -> None:
 def test_login(page: Page):
     page.goto('https://sandbox2.syncwise360.com/login')
 
-    page.locator("//input[@id='username']").type("adminexamplecom")
-    page.locator("//input[@id='username']").clear()
+    # page.locator("//input[@id='username']").type("adminexamplecom")
+    # page.locator("//input[@id='username']").clear()
+    #
+    # page.locator("//input[@id='username']").fill("adminexamplecom")
+    # page.locator("//input[@id='username']").clear()
 
-    page.locator("//input[@id='username']").fill("adminexamplecom")
-    page.locator("//input[@id='username']").clear()
+    page.locator("//input[@id='username']").fill("eighteendev")
+    page.locator("//input[@id='password']").fill("Qwerty01!")
+    element = page.locator('//button[@aria-label="submit"]')
+    # element.screenshot(path="screenshots/screenshot.png")
+    element.click()
+    print("PRESS BUTTON SUBMIT")
 
-    page.locator("//input[@id='username']").press_sequentially("qwerty", delay=200)
+    page.locator('//button[@aria-label="profile-icon"]').click(timeout=30000)
+    print("PRESS BUTTON PROFILE")
+
+    time.sleep(5)
+
+
 
 
 def test_checkbox(page):
@@ -46,30 +58,25 @@ def test_drag_and_drop(page):
 
 
 
-# def test_dialogs(page: Page):
-#     page.goto("https://zimaev.github.io/dialog/")
-#     page.on("dialog", lambda dialog: dialog.accept())
-#     page.on("dialog", lambda dialog: print(dialog.message))
-#     page.get_by_text("Диалог Confirmation").click()
-
-
 def test_dialogs(page: Page):
     page.goto("https://zimaev.github.io/dialog/")
-
-    # Создаем переменную для хранения сообщения
-    dialog_message = None
-
-    # Обработчик диалога
-    def handle_dialog(dialog):
-        nonlocal dialog_message  # Используем nonlocal для изменения переменной в замыкании
-        dialog_message = dialog.message
-        dialog.accept()
-
-    # Подписываемся на событие диалога
-    page.on("dialog", handle_dialog)
-
-    # Вызываем диалог
+    page.on("dialog", lambda dialog: dialog.accept())
+    page.on("dialog", lambda dialog: print(dialog.message))
     page.get_by_text("Диалог Confirmation").click()
 
-    # Выводим сообщение из диалога
-    print("Сообщение из диалога:", dialog_message)
+
+# def test_dialogs(page: Page):
+#     page.goto("https://zimaev.github.io/dialog/")
+#     # Создаем переменную для хранения сообщения
+#     dialog_message = None
+#     # Обработчик диалога
+#     def handle_dialog(dialog):
+#         nonlocal dialog_message  # Используем nonlocal для изменения переменной в замыкании
+#         dialog_message = dialog.message
+#         dialog.accept()
+#     # Подписываемся на событие диалога
+#     page.on("dialog", handle_dialog)
+#     # Вызываем диалог
+#     page.get_by_text("Диалог Confirmation").click()
+#     # Выводим сообщение из диалога
+#     print("Сообщение из диалога:", dialog_message)
