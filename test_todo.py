0f332f764bfa8928f5885bd2e9849107b91c40c2
FILA <fila090580@gmail.com>
2025-03-16 02:50:12 +0200

    new

--- test_todo.py
+++ test_todo.py
@@ -1,23 +1,23 @@
-import re
 import time
 
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+def test_add_todo(page) -> None:
+    # browser = page.chromium.launch(headless=False)
+    # context = browser.new_context()
+    # page = context.new_page()
     page.goto("https://demo.playwright.dev/todomvc/#/")
     page.get_by_placeholder("What needs to be done?").click()
     page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
     page.get_by_placeholder("What needs to be done?").press("Enter")
     page.get_by_role("checkbox", name="Toggle Todo").check()
+    time.sleep(3)
 
     # ---------------------
-    context.close()
-    browser.close()
+    # context.close()
+    # browser.close()
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+# with sync_playwright() as playwright:
+#     run(playwright)
