6f30a0c32cd43f8838a6541aa8e8adc4093adf7d
FILA <fila090580@gmail.com>
2025-03-19 23:52:30 +0200

    new

--- tests/first_/conftest.py
+++ tests/first_/conftest.py
@@ -3,13 +3,33 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 
-# def pytest_fixture_setup(fixturedef, request):
-#     print(f"Фикстура {fixturedef.scope} для {fixturedef.name} запускается")
+# @pytest.fixture(scope="session")
+# def browser_context_args():
+#     print("browser_context")
+#     return {
+#
+#         "viewport": {
+#             "width": 1920,
+#             "height": 1080,
+#         }
+#     }
+#
+#
+# @pytest.fixture
+# def custom_page(browser_context_args):
+#     print("__PAGE__START__")
+#     with sync_playwright() as playwright:
+#         browser = playwright.chromium.launch(headless=False)
+#         context = browser.new_context(**browser_context_args)  # Используем аргументы
+#         page = context.new_page()
+#         yield page
+#         page.close()
+#         browser.close()
+#     print("__PAGE__FINISH__")
 
 
 @pytest.fixture(scope="session")
-def browser_context_args():
-    print("browser_context")
+def browser_context_args(browser_context_args):
     return {
 
         "viewport": {
@@ -20,37 +40,13 @@ def browser_context_args():
 
 
 @pytest.fixture
-# def browser_fixture():
-def page(browser_context_args):
-    print("__PAGE__START__")
+def browser_fixture():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(**browser_context_args)  # Используем аргументы
+        context = browser.new_context()
         page = context.new_page()
         yield page
         page.close()
         browser.close()
-    print("__PAGE__FINISH__")
-
-# @pytest.fixture(scope="session")
-# def browser_context_args(browser_context_args):
-#     return {
-#
-#         "viewport": {
-#             "width": 1920,
-#             "height": 1080,
-#         }
-#     }
-
-
-# @pytest.fixture
-# def browser_fixture():
-#     with sync_playwright() as playwright:
-#         browser = playwright.chromium.launch(headless=False)
-#         context = browser.new_context()
-#         page = context.new_page()
-#         yield page
-#         page.close()
-#         browser.close()
 
 
--- tests/first_/test_first.py
+++ tests/first_/test_first.py
@@ -1,10 +1,12 @@
+from playwright.sync_api import Page, expect
 
 
-def test_add_todo_first(page) -> None:
+def test_add_todo_first(page: Page) -> None:
     print("----------------FIRST-------------")
     page.goto("https://demo.playwright.dev/todomvc/#/")
     page.get_by_placeholder("What needs to be done?").click()
     page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
     page.get_by_placeholder("What needs to be done?").press("Enter")
     page.get_by_role("checkbox", name="Toggle Todo").check()
+
 
